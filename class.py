37bce661d02755cc8877994fa7d2ef115486625a
lukas9318 <luc.holub@gmail.com>
2022-12-30 23:19:34 +0100

    Adding func3

--- class.py
+++ class.py
@@ -1,9 +1,12 @@
 class Dev():
   def __init__(self):
     pritn("Class Dev")
-    
+
   def fun1(self,a,b):
     return a+b
-  
+
   def fun2(self,a,b):
     return a-b
+
+  def fun3(self,a,b):
+    return a*b
